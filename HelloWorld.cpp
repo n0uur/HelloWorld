aa5c1712d8226141182c527ebb50d14d4878ad7f
n0uur <50010805+n0uur@users.noreply.github.com>
2019-05-09 04:06:27 +0700

    unnecessary

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	
-  std::setlocale(LC_ALL, "th_TH.UTF-8");
+  setlocale(LC_ALL, "th_TH.UTF-8");
   //char Text[] = { 'H','E','L','L','O',' ', 'W', 'O','R','L','D' }; // NEED TO BE THAI :/
   char Text[] = { 'ส','ว','ั','ส','ด','ี',' ', 'โ', 'ล','ก','!' };
   for(int i = 0;i< sizeof(Text);i++)
